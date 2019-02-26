84f84a19063ecfcfc433cd7ad014e80592627be6
Kylan Gibbs <Kylan.Gibbs@Bain.com>
2019-02-26 18:05:17 +0000

    new db

--- manage.py
+++ manage.py
@@ -1,9 +1,12 @@
+import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
 from app import app, db
 
-migrate = Migrate(app, db,directory="/app/migrations/")
+app.config.from_object(os.environ['APP_SETTINGS'])
+
+migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
