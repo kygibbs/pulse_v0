977ed3a0ddb5091bf0cc882531eaa50dadef6f1d
kygibbs <kylan.gibbs@gmail.com>
2019-04-06 21:06:36 +0100

    m

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def receive_message():
                         m = message['message']['text']
 
                         # key = check_key(rating,command)
-                        if len(User.query.filter_by(user=str(recipient_id)).all()))>0:
+                        if len(User.query.filter_by(user=str(recipient_id)).all())>0:
                             Proliferate(recipient_id,m)
                         else:
                             bot.send_text_message(recipient_id,"please add a username with 'set name'")
