14f1614bc20ccdfbdbea09d7498dbf446adfcf20
kygibbs <kylan.gibbs@gmail.com>
2019-04-06 18:24:16 +0100

    new peak

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wrgfrthoobosxz:da8d915c0fafc
 
 db = SQLAlchemy(app)
 
+# XXX: 
 from models import Message, User, Rating, Follower
 
 #We will receive messages that Facebook sends our bot at this endpoint
